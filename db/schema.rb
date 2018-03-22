86c5631d20389b54c610acde93489b5408a47b68
Yen Minh <minhhy2897@gmail.com>
2018-03-22 11:12:26 +0700

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 20180321064437) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
